39db63a169d9cf03470f2d7b7c1eda1d599b5cae
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-10-20 19:00:42 -0500

    feat: :sparkles: Agrega Lista de Tareas

---
+++ App.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheContador />
+  <ListaTareas />
+</template>
+
+<script setup>
+import TheContador from '@/components/TheContador.vue';
+import ListaTareas from '@/components/ListaTareas.vue';
+</script>
+
+<style scoped></style>
---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="handleAddTarea">
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">{{ tarea }}</li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const handleAddTarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = "";
+}
+
+</script>
+<style></style>
